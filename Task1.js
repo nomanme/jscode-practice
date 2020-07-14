0648b68d33a871b228076c463161d313d283cb9b
Mohammad-Abdullah-Al-Noman <abdullah.noman083@gmail.com>
2020-07-14 20:16:16 +0600

    js-pactice-code

---
+++ Task1.js
@@ -0,0 +1,5 @@
+/* Program for Task 1*/
+// A program to print your details info
+document.write("Name : Mohammad Noman <br/>")
+document.write("Nationality : Bangladeshi <br/>")
+document.write("Email : mohammad.me@gmail.com")
